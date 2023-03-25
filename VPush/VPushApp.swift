4b2f9f6841f21384f1fe2eccc215765556dafe01
Andrii Vysher <andrey.vysher@emmacare.com>
2023-03-25 20:20:49 +0100

    Init commit

---
+++ VPush/VPushApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPushApp.swift
+//  VPush
+//
+//  Created by Andrii Vysher on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct VPushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
